a7a2a9e966a1ec7e20736c34f2a6f0e90ba56083
luo <luokobe@gmail.com>
2014-05-23 12:49:14 +0800

    some changes

--- src/draw2.js
+++ src/draw2.js
@@ -6,14 +6,12 @@
         pb.stage = new createjs.Stage(canvas);
         pb.shapeType = 'rect';
         pb.strokeColor = '#f00';
-        pb.color = '#f00';
         pb.textSize = '24px';
         pb.textFamily = "Arial";
         pb.strokeSize = 8;
         pb.creating = false;
         pb.hoverShape = false;
         pb.modifyText = false;
-        pb.shapeType = null;
         pb.mouseX = 0;
         pb.mouseY = 0;
         pb.stageX = 0;
@@ -22,11 +20,119 @@
         init();
 
         function init() {
-            pb.stage.addEventListener('stagemousedown', function(e) {
-                var s = new Shape(e.stageX, e.stageY);
-                pb.stage.addChild(s.shape);
+            pb.stage.enableMouseOver(10);
+            pb.stage.on('stagemousedown', mouseDown, false);
+        }
+
+        function mouseDown(e) {
+            console.log(!pb.shapeType);
+            if (!pb.shapeType) {
+                return;
+            } else if (pb.hoverShape) {
+                return;
+            }
+
+            pb.creating = true;
+
+            var originalX = e.stageX,
+                originalY = e.stageY;
+
+            switch (pb.shapeType) {
+                case 'rect':
+                    var s = new Rect(e.stageX, e.stageY);
+                    break;
+                case 'ellipse':
+                    var s = new Ellipse(e.stageX, e.stageY);
+                    break;
+                case 'r-rect':
+                    var s = new RoundRect(e.stageX, e.stageY);
+                    break;
+                case 'line':
+                    var s = new Line(e.stageX, e.stageY);
+                    break;
+                case 'arrow':
+                    var s = new Arrow(e.stageX, e.stageY);
+                    break;
+                case 'free-line':
+                    var s = new FreeLine(e.stageX, e.stageY);
+                    break;
+                case 'free-arrow':
+                    var s = new FreeArrow(e.stageX, e.stageY);
+                    break;
+                case 'text':
+                    if (document.getElementById('temp_input').style.display == 'none') {
+                        pb.textX = e.stageX;
+                        pb.textY = e.stageY;
+                    }
+                    pb.creating = false;
+                    break;
+            }
+
+            if (!s) {
+                return;
+            }
+
+            console.log(s);
+
+            pb.stage.addChild(s.shape);
+
+            pb.stage.addEventListener('stagemousemove', mousemove, false);
+            pb.stage.addEventListener('stagemouseup', mouseup, false);
+
+            function mousemove(e) {
+                console.info('stagemousemove');
+                if (s._type == 'shape') {
+                    s.bounds.width = e.stageX - s.bounds.x;
+                    s.bounds.height = e.stageY - s.bounds.y;
+                } else if (s._type == 'line') {
+                    s.endX = e.stageX;
+                    s.endY = e.stageY;
+                } else if (s._type == 'free') {
+                    s.points.push({
+                        x: e.stageX,
+                        y: e.stageY
+                    });
+                    var angle = Math.atan2(e.stageY - s._oY, e.stageX - s._oX) * 180 / Math.PI;
+
+                    setTimeout(function() {
+                        s._oX = e.stageX;
+                        s._oY = e.stageY;
+                    }, 100);
+
+                    s.endX = e.stageX;
+                    s.endY = e.stageY;
+                }
+
+                s.rePaint(angle);
+
+
+
                 pb.stage.update();
-            });
+            }
+
+            function mouseup(e) {
+                if (pb.stage.mouseInBounds) {
+                    if (e.stageX == originalX && e.stageY == originalY) {
+                        pb.stage.removeChild(s);
+                    }
+                    pb.creating = false;
+
+                    pb.stage.off('stagemousemove', mousemove, false);
+                }
+            }
+        }
+
+        
+
+
+
+        /******Utils******/
+        function extend(Child, Parent) {
+            var F = function() {};　　　　
+            F.prototype = Parent.prototype;　　　　
+            Child.prototype = new F();　　　　
+            Child.prototype.constructor = Child;　　　　
+            Child.uber = Parent.prototype;
         }
 
         /*******Shapes*******/
@@ -41,6 +147,7 @@
             this._startX = 0;
             this._startY = 0;
             this.strokeColor = pb.strokeColor;
+            this.strokeSize = pb.strokeSize;
             this.textSize = pb.textSize;
             this.shape = new createjs.Shape();
             this._type = 'shape';
@@ -55,9 +162,6 @@
             s.cursor = 'move';
             s.hitArea = new createjs.Shape();
 
-            s.graphics.beginFill("#ff0000").drawRect(0, 0, 100, 100);
-            s.hitArea.graphics.beginFill("#ff0000").drawRect(0, 0, 100, 100);
-
             s.addEventListener('mouseover', function() {
                 console.info('shape mouseover');
                 pb.hoverShape = true;
@@ -70,8 +174,20 @@
                 s.shadow = null;
                 s.getStage().update();
             }, false);
+        };
 
 
+        /****Rectangle***/
+        function Rect(x, y) {
+            Shape.apply(this, arguments);
+        };
+
+        extend(Rect, Shape);
+
+        Rect.prototype.rePaint = function() {
+            var z = this;
+            z.shape.graphics.clear().setStrokeStyle(this.strokeSize, "round", "round").beginStroke(this.strokeColor).drawRect(z.bounds.x, z.bounds.y, z.bounds.width, z.bounds.height);
+            z.shape.hitArea.graphics.clear().beginFill("#FFF").drawRect(z.bounds.x, z.bounds.y, z.bounds.width, z.bounds.height);
         };
 
 
